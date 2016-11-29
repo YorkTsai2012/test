5cd24b0902dc477bf526ebd430a81465fcff86dd
caiyueqiao <caiyueqiao@ucloud.cn>
2016-11-29 16:57:00 +0800

    crc32 命令  cksum 从文件中取字符串会多取一个什么字符?

--- cpp-dev/tools/crc32.c
+++ cpp-dev/tools/crc32.c
@@ -2,10 +2,18 @@
 #include <stdio.h>
 #include <zlib.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
+
+    if (argc < 2) {
+        printf("usage: %s %s\n", argv[0], "input");
+        exit(-1);
+    }
+
     uLong crcKey = crc32(0L, Z_NULL, 0);
-    char * volumeid = "redeploy";
+    char * volumeid = argv[1];
+    printf("%s %d\n", volumeid, strlen(volumeid));
     crcKey = crc32(crcKey, (Bytef*)volumeid, strlen(volumeid));
     printf("crc32 redeploy %lu %%100=%lu\n", crcKey, crcKey % 100);
 }
