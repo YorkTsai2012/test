1b932da4f28cca3b11def754e192ec29a922bfe7
caiyueqiao <caiyueqiao@ucloud.cn>
2016-12-16 13:36:25 +0800

    调用write 写一个整型变量 4个字节 也是小端的顺序

--- cpp-dev/endian.cpp
+++ cpp-dev/endian.cpp
@@ -1,5 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 void printInt(int n) {
     uint8_t *p = (uint8_t*)&n;
@@ -8,13 +10,29 @@ void printInt(int n) {
     }
 }
 
+ssize_t writeToFile(const char* path, int n) {
+    int fd = open(path, O_WRONLY|O_CREAT);
+    ssize_t ret = write(fd, (const void*)&n, sizeof(int));
+    if (ret < 0) {
+        perror("write error");
+    }
+    close(fd);
+    return ret;
+}
+
+//TODO  向文件流中写一个整型 
+
 int main(int argc, char *argv[]) {
 
     //小端 低位字节先存到低地址   大端  高位字节先存低地址 （阅读顺序）
-    int a = 0x7f451234; //先存 0x34 -> 0x12 -> 0x45 -> 0x7f
+    int a = 0x7f454c46; //先存 0x46 -> 0x4c -> 0x45 -> 0x7f
     printInt(a);
     int b = 0x7f45;  // 先存 0x45 -> 0x7f -> 0x00 -> 0x00
     printInt(b);
+
+    //写到文件的 存储顺序 和打印顺序一致
+    writeToFile("7f454c46.save", a);
+    writeToFile("7f45.save", b);
 
     return 0;
 }
