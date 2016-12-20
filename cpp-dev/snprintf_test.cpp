41a0e1228af37fbe9d107dc1035dfefbde15984f
caiyueqiao <caiyueqiao@ucloud.cn>
2016-12-20 20:51:19 +0800

    snprintf截断后会添加\0

--- cpp-dev/snprintf_test.cpp
+++ cpp-dev/snprintf_test.cpp
@@ -1,12 +1,23 @@
 
+/*
+
+linux下的snprintf截断后会添加'\0'
+
+*/
+
 #include <stdio.h>
 #include <stdint.h>
 
 int main(int argc, char* argv[]) {
 
-	uint32_t m_current_topid = 200000030;
-	char topid[32];
-	snprintf(topid, sizeof(topid)-1, "%u", m_current_topid);
-	printf("topid=%s\n", topid);
+	uint32_t m_current_topid = 30;
+	char topid[2];
+	int ret = snprintf(topid, sizeof(topid), "%u", m_current_topid);
+	printf("ret:%d|topid:%s\n", ret, topid);
+
+    for (int i = 0 ; i < sizeof(topid)/sizeof(topid[0]); i++) {
+        printf("topid[%d]=%d\n", i, topid[i]);
+    }
+
 	return 0;
 }
