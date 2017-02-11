9b62c0c91460f5f8671d5be297f974f0a364630b
caiyueqiao <caiyueqiao@ucloud.cn>
2017-02-11 20:00:32 +0800

    leveld demo; c++ object memory layout

---
+++ cpp-dev/leveldb/leveldb_demo.cpp
@@ -0,0 +1,68 @@
+/* make
+ * g++ -g leveldb_demo.cpp -I/root/leveldb/include -L/root/leveldb/out-shared -lleveldb
+ * ref
+ * https://rawgit.com/google/leveldb/master/doc/index.html
+ */
+
+#include <cassert>
+#include "leveldb/db.h"
+#include "leveldb/write_batch.h"
+
+int main(int argc, char* argv[]) {
+    leveldb::DB* db;
+    leveldb::Options options;
+    options.create_if_missing = true;
+    //options.error_if_exists = true;
+    leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
+    //assert(status.ok());
+    printf("Open|status: %s\n", status.ToString().c_str());
+
+    //basice operation: Put, Get and Delete
+    std::string value = "value1";
+    std::string key1 = "key1";
+    std::string key2 = "key2";
+    leveldb::Status s = db->Put(leveldb::WriteOptions(), key1, value);
+    if (s.ok()) {
+        printf("Put |key: %s|value: %s|status: %s\n", key1.c_str(), value.c_str(), status.ToString().c_str());
+    }
+    s = db->Get(leveldb::ReadOptions(), key1, &value);
+    if (s.ok()) {
+        printf("Get |key: %s|value: %s|status: %s\n", key1.c_str(), value.c_str(), status.ToString().c_str());
+    }
+    s = db->Put(leveldb::WriteOptions(), key2, value);
+    if (s.ok()) {
+        printf("Put |key: %s|value: %s|status: %s\n", key2.c_str(), value.c_str(), status.ToString().c_str());
+    }
+    s = db->Delete(leveldb::WriteOptions(), key2);
+    if (s.ok()) {
+        printf("Delete |key: %s|value: %s|status: %s\n", key1.c_str(), value.c_str(), status.ToString().c_str());
+    } else {
+        printf("Delete Error|key: %s|value: %s|status: %s\n", key1.c_str(), value.c_str(), status.ToString().c_str());
+    }
+
+    //批量操作
+    s = db->Get(leveldb::ReadOptions(), key1, &value);
+    if (s.ok()) {
+        leveldb::WriteBatch batch;
+        batch.Delete(key1);
+        batch.Put(key2, value);
+        s = db->Write(leveldb::WriteOptions(), &batch);
+        if (s.ok()) {
+            printf("Batch Delete(key1) Put(key2,value)|key1: %s|key2: %s|value: %s|status: %s\n",
+                key1.c_str(), key2.c_str(), value.c_str(), status.ToString().c_str());
+        }
+    }
+
+    //遍历
+    leveldb::ReadOptions readOptions;
+    readOptions.snapshot = db->GetSnapshot();
+    leveldb::Iterator* it = db->NewIterator(readOptions);
+    for (it->SeekToFirst(); it->Valid(); it->Next()) {
+        printf("Iterate|key:%s|value:%s|status:%s\n",
+            it->key().ToString().c_str(), it->value().ToString().c_str(), it->status().ToString().c_str());
+    }
+    delete it;
+    db->ReleaseSnapshot(readOptions.snapshot);
+
+    delete db;
+}
---
+++ cpp-dev/memery_layout/diamond_inheritance.cpp
@@ -0,0 +1,173 @@
+/*
+ *   菱形继承内存储布局
+ *   http://m.blog.csdn.net/article/details?id=49102373
+ */
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+
+/*
+class  A;
+class  B : virtual public A;
+class  C : virtual public A;
+class  D : public B , public C
+*/
+
+//单一的虚拟继承
+//引例一：
+/*
+    //{_a = -6672}
+class Base {
+private:
+    int _a;
+};
+
+    //{<Base> = {_a = 4195712}, _vptr.Derived = 0x400838}
+class Derived: virtual public Base {
+
+};
+*/
+
+//引例二：
+//{_vptr.Base = 0x400cb0, _a = 4196400}
+/*
+class Base {
+public:
+    virtual void funl() {
+        cout << "Base::fun1()" << endl;
+    }
+private:
+    int _a;
+};
+
+//{<Base> = {_vptr.Base = 0x400c80, _a = -148141152}, _vptr.Derived = 0x400c58}
+class Derived: virtual public Base {
+public:
+    virtual void funl() {
+        cout << "Derived::fun1()" << endl;
+    }
+    virtual void Derivedfun2() {
+        cout << "Derived::Derivedfun2()" << endl;
+    }
+};
+*/
+
+class Base {
+public:
+    virtual void func1() {
+        cout << "Base::func1()" << endl;
+    }
+    virtual void Basefunc2() {
+        cout << "Base::func2()" << endl;
+    }
+private:
+    int b;
+};
+
+//class Base1 :public virtual Base {
+class Base1: public Base {
+public:
+        virtual void func1()
+       {
+              cout << "Base1::func1()" << endl;
+       }
+        virtual void Base1func2()
+       {
+              cout << "Base1::func2()" << endl;
+       }
+private:
+        int b1;
+};
+
+//class Base2: public virtual Base
+class Base2: public Base {
+public:
+        virtual void func1()
+       {
+              cout << "Base2::func1()" << endl;
+       }
+        virtual void Base2func2()
+       {
+              cout << "Base2::func2()" << endl;
+       }
+private:
+        int b2;
+};
+
+typedef void (*FUNC)();
+void PrintVTable(int *VTable)
+{
+       cout << "虚表地址：" << VTable << endl;
+        for (int i = 0; VTable[i] != 0; i++)
+       {
+              printf( "第VTable[%d]是：OX%x->" , i, VTable[i]);
+               FUNC p = (FUNC )VTable[i];
+              p();
+       }
+       cout << endl;
+}
+
+class Derived :  public Base1, public Base2
+{
+public:
+        virtual void func1()
+       {
+              cout << "Derived::func1()" << endl;
+       }
+        virtual void Derivedfunc2()
+       {
+              cout << "Derived::func2()" << endl;
+       }
+        virtual void Derivedfunc3()
+       {
+              cout << "Derived::func3()" << endl;
+       }
+private:
+        int d1;
+};
+
+int main(int argc, char* argv[]) {
+
+    char c = 'c';
+    char *p = &c;
+    printf("sizeof(point) = %lu\n", sizeof(p));
+
+    Base b;
+    Base1 b1;
+    Base2 b2;
+    Derived d;
+    printf("sizeof(b) = %lu\n", sizeof(b));
+    printf("sizeof(b1) = %lu\n", sizeof(b1));
+    printf("sizeof(b2) = %lu\n", sizeof(b2));
+    printf("sizeof(d) = %lu\n", sizeof(d));
+
+    //Base的对象b:
+    int *VTable = (int *)(*(int *)&b);
+    PrintVTable(VTable);    //Base的虚函数表
+}
+
+/*
+(gdb) p b
+$1 = {_vptr.Base = 0x401330, b = 4196480}
+(gdb) print sizeof(b)
+$2 = 16
+(gdb) p b1
+$3 = {<Base> = {_vptr.Base = 0x4012e8, b = 4198000}, _vptr.Base1 = 0x4012b8, b1 = 4198069}
+(gdb) p sizeof(b1)
+$4 = 32
+(gdb) p b2
+$5 = {<Base> = {_vptr.Base = 0x401268, b = 4196291}, _vptr.Base2 = 0x401238, b2 = 4198000}
+(gdb) p sizeof(b2)
+$6 = 32
+(gdb) p d
+$7 = {<Base1> = {<error reading variable>
+(gdb) next
+135     printf("sizeof(b) = %lu\n", sizeof(b));
+(gdb) p d
+$8 = {<Base1> = {<Base> = {_vptr.Base = 0x401100, b = 4198182}, _vptr.Base1 = 0x401098, b1 = 65535}, <Base2> = {_vptr.Base2 = 0x4010d0, b2 = 4197156}, d1 = 0}
+(gdb) p sizeof(d)
+$9 = 48
+*/
