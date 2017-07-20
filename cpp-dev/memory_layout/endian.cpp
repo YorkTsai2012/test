2449eaabc04eef3cdc5717682d4164a2661aa680
YorkTsai <york.cai@ucloud.cn>
2017-07-20 15:34:16 +0800

    小端 8字节 18680221174 -> 00000004596d8df6  低位字节先存 f68d6d5904000000

--- cpp-dev/memory_layout/endian.cpp
+++ cpp-dev/memory_layout/endian.cpp
@@ -1,16 +1,17 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 
 #include <stdint.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 
-void printInt(int n) {
+void printInt(uint64_t n) {
     uint8_t *p = (uint8_t*)&n;
-    for (int i = 0; i < sizeof(int); i++) {
-        printf("order:%x|value:%x\n", i, p[i]);
+    for (int i = 0; i < sizeof(n); i++) {
+        printf("order:%d|value:%02x\n", i, p[i]);
     }
 }
 
@@ -48,14 +49,9 @@ void readFromFileStream(const char* path) {
 
 //TODO  向文件流中写一个整型 
 
-int main(int argc, char *argv[]) {
-
-    //小端 低位字节先存到低地址   大端  高位字节先存低地址 （阅读顺序）
+void TestFile() {
     int a = 0x7f454c46; //先存 0x46 -> 0x4c -> 0x45 -> 0x7f
-    printInt(a);
     int b = 0x7f45;  // 先存 0x45 -> 0x7f -> 0x00 -> 0x00
-    printInt(b);
-
     //写到文件的 存储顺序 和打印顺序一致
     writeToFile("7f454c46.file", a);
     writeToFile("7f45.file", b);
@@ -63,6 +59,25 @@ int main(int argc, char *argv[]) {
     //但是按照文件流来写就不是预期的字节了 似乎有编码
     writeToFileStream("7f454c46.stream", a);
     readFromFileStream("7f454c46.stream");
+}
+
+void TestInt() {
+    //小端 低位字节先存到低地址   大端  高位字节先存低地址 （阅读顺序）
+    int a = 0x7f454c46; //先存 0x46 -> 0x4c -> 0x45 -> 0x7f
+    printInt(a);
+    int b = 0x7f45;  // 先存 0x45 -> 0x7f -> 0x00 -> 0x00
+    printInt(b);
+}
+
+void TestUint64() {
+    //printf "%lu\n" "0x00000004596d8df6"
+    uint64_t n =  18680221174;
+    printInt(n);
+}
+
+int main(int argc, char *argv[]) {
+
+    TestUint64();
 
     return 0;
 }
