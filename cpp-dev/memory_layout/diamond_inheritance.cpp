8d3469181e5b0db70551bdc104a8c18e1fc4068f
YorkTsai <york.cai@ucloud.cn>
2017-07-24 20:26:40 +0800

    gcc 内存布局 虚继承+虚函数 共用同一个指针 vptr

--- cpp-dev/memory_layout/diamond_inheritance.cpp
+++ cpp-dev/memory_layout/diamond_inheritance.cpp
@@ -6,11 +6,15 @@
     若派生类中含有新的虚函数（改写的不算），
     将会自己产生一个指向自己虚表的指针。**
 
-- 如果普通虚继承，虚表指针也被派生类继承；
+- 如果普通继承，虚表指针也被派生类继承；
 如果是虚继承，派生类有新的虚函数（不包括改写的），
 将会有自己的虚表指针。
 
 实际测试，只要是虚继承，基类有虚函数，派生类就有自己的虚表指针
+
+另外 有实际验证 虚继承的实现机制 
+
+http://blog.csdn.net/freeelinux/article/details/53872211
 
  */
 
@@ -82,40 +86,37 @@ private:
 //Mac OS X中的GDB {<Base> = {_vptr$Base = 0x100002190 <vtable for Base1+16>, b = 140734799804768}, b1 = 0}
 //可以
 //class Base1 :public virtual Base {
-class Base1: public Base {
+class Base1: virtual public Base {
+
 public:
-        virtual void func1()
-       {
-              cout << "Base1::func1()" << endl;
-       }
-        virtual void Base1func2()
-       {
-              cout << "Base1::func2()" << endl;
-       }
+    virtual void func1() {
+        cout << "Base1::func1()" << endl;
+    }
+
+    virtual void Base1func2() {
+        cout << "Base1::func2()" << endl;
+    }
 private:
-       int64_t b1;
+    int64_t b1;
 };
 
 //{<Base> = {_vptr$Base = 0x100002200 <vtable for Base2+56>, b = 4294967296}, _vptr$Base2 = 0x1000021e0 <vtable for Base2+24>, b2 = 1}
 class Base2: public virtual Base {
-//class Base2: public Base {
 public:
 
 /*
-        virtual void func1()
-       {
-              cout << "Base2::func1()" << endl;
-       }
+    virtual void func1() {
+        cout << "Base2::func1()" << endl;
+    }
 */
 
 /*
-        virtual void Base2func2()
-       {
-              cout << "Base2::func2()" << endl;
-       }
+    virtual void Base2func2() {
+        cout << "Base2::func2()" << endl;
+    }
 */
 private:
-        int64_t b2;
+    int64_t b2;
 };
 
 typedef void (*FUNC)();
@@ -131,23 +132,24 @@ void PrintVTable(int *VTable)
        cout << endl;
 }
 
-class Derived :  public Base1, public Base2
-{
+class Derived :  virtual public Base1, virtual public Base2 {
+
 public:
-        virtual void func1()
-       {
-              cout << "Derived::func1()" << endl;
-       }
-        virtual void Derivedfunc2()
-       {
-              cout << "Derived::func2()" << endl;
-       }
-        virtual void Derivedfunc3()
-       {
-              cout << "Derived::func3()" << endl;
-       }
+
+    virtual void func1() {
+        cout << "Derived::func1()" << endl;
+    }
+
+    virtual void Derivedfunc2() {
+        cout << "Derived::func2()" << endl;
+    }
+
+    virtual void Derivedfunc3() {
+        cout << "Derived::func3()" << endl;
+    }
+
 private:
-        int64_t d1;
+    int64_t d1;
 };
 
 int main(int argc, char* argv[]) {
@@ -164,10 +166,6 @@ int main(int argc, char* argv[]) {
     printf("sizeof(b1) = %lu\n", sizeof(b1));
     printf("sizeof(b2) = %lu\n", sizeof(b2));
     printf("sizeof(d) = %lu\n", sizeof(d));
-
-    //Base的对象b:
-    int *VTable = (int *)(*(int *)&b);
-    PrintVTable(VTable);    //Base的虚函数表
 }
 
 /*
