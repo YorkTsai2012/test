17c8df7b1ed3e1378da8e19ecc671e8e67c5d3b3
蔡月樵 <caiyueqiao@163.com>
2019-03-12 23:26:49 +0800

    Update test-upper-bound.cpp

--- cpp-dev/test-upper-bound.cpp
+++ cpp-dev/test-upper-bound.cpp
@@ -1,4 +1,16 @@
+/*
+语义
+lower_bound -> not go before key :  >= key
+upper_bound -> go after key :  > key
 
+与左闭右开区间表达一致
+[, )
+
+这样多个区间，构成一个完整的集合。
+
+*/
+
+//g++ test_lower_upper_bound.cc --std=c++11
 // map::lower_bound/upper_bound
 #include <iostream>
 #include <map>
@@ -14,19 +26,21 @@ int main ()
   mymap['d']=80;
   mymap['e']=100;
 
-  itlow=mymap.lower_bound ('b');  // itlow points to b
-  itup=mymap.upper_bound ('e');   // itup points to e (not d!)
+  itlow = mymap.lower_bound ('b');  // itlow points to b
+  itup = mymap.upper_bound ('e');   // itup points to end
+
+  std::cout << "lower_bound " << itlow->first
+            << " => " << itlow->second << '\n';
 
   if (itup == mymap.end()) {
     std::cout << "the map::end" << "\n";
-   }
-  std::cout << itup->first << " => " << itup->second << '\n';
-  //mymap.erase(itlow,itup);        // erases [itlow,itup)
+  }
 
-/*
-  // print content:
-  for (std::map<char,int>::iterator it=mymap.begin(); it!=mymap.end(); ++it)
-    std::cout << it->first << " => " << it->second << '\n';
-*/
+  mymap.erase(itlow,itup);        // erases [itlow,itup)
+
+  // print content
+  for (auto& it: mymap) {
+    std::cout << it.first << " => " << it.second << '\n';
+  }
   return 0;
 }
